b5227a93779b3207f6769e444d30d842981c267e
Meforgers <45572414+MeforgersDev@users.noreply.github.com>
2024-07-11 13:54:01 +0300

    Update activations.py

--- activations.py
+++ activations.py
@@ -15,3 +15,33 @@ class ReLU(Function):
         grad_input = grad * (x.data > 0)
         if x.requires_grad:
             x.backward(grad_input)
+
+class Sigmoid(Function):
+    @staticmethod
+    def forward(x):
+        result = 1 / (1 + np.exp(-x.data))
+        result = Tensor(result, requires_grad=x.requires_grad)
+        result._grad_fn = Sigmoid
+        return result
+
+    @staticmethod
+    def backward(grad):
+        x, = Sigmoid.saved_tensors
+        grad_input = grad * (1.0 - x.data) * x.data
+        if x.requires_grad:
+            x.backward(grad_input)
+
+class Tanh(Function):
+    @staticmethod
+    def forward(x):
+        result = np.tanh(x.data)
+        result = Tensor(result, requires_grad=x.requires_grad)
+        result._grad_fn = Tanh
+        return result
+
+    @staticmethod
+    def backward(grad):
+        x, = Tanh.saved_tensors
+        grad_input = grad * (1 - np.square(x.data))
+        if x.requires_grad:
+            x.backward(grad_input)
